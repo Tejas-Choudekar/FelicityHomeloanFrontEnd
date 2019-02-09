c65fe4c65c5f293730b48964f879b28ccb0b4741
Tejas-Choudekar <tejus.choudekar@gmail.com>
2019-02-09 14:48:33 +0530

    intial project setup

---
+++ src/app/EligibiltyCalculator/EligibilityCalculatorValue.ts
@@ -0,0 +1,9 @@
+export class EligibilityCalculatorValue{
+    constructor(
+        public monthlyIncome?: number,
+        public rateOfInterest?:number,
+        public eligibility?:number
+    ){
+
+    }
+}
